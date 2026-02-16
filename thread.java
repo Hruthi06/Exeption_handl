a057a2de7f941650ba4dcee5b40666977653bc71
Hruthik K R <yourgithubemail@gmail.com>
2026-02-16 14:11:03 +0530

    done threading

--- thread.java
+++ thread.java
@@ -37,3 +37,59 @@ class try1 {
         t2.start();
     }
 }
+
+class try2{
+    public static void main(String[] args){
+        Runnable r1=new Runnable(){
+            public void run(){
+                System.out.println("Hello");
+            }
+        };
+        Thread t1=new Thread(r1);
+        t1.start();
+    }
+}
+
+
+class try3{
+    public static void main(String[] args){
+        Runnable r1 = () -> {
+            System.out.println("Hello");
+        };
+        Thread t1 = new Thread(r1);
+        t1.start();
+    }
+}
+
+
+class try4{
+    int count = 0;
+    synchronized void increment() {
+        count++;
+    }}
+
+class try5{
+    public static void main(String[] args)  throws InterruptedException{
+        try4 t = new try4();
+        Thread t1 = new Thread(){
+            public void run(){
+                for (int i = 0; i < 1000; i++) {
+                    t.increment();
+                }
+            }
+        };
+        Thread t2 = new Thread(){
+            public void run(){
+                for (int i = 0; i < 1000; i++) {
+                    t.increment();
+                }
+            }
+        };
+        t1.start();
+        t2.start();
+        t1.join();
+        t2.join();
+        System.out.println("Count :"+t.count);
+    }
+}
+
